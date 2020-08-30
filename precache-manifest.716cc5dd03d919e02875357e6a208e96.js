2c14ef5318648afacf80bb8ef140b6b6c31e052e
Chazz Horn <Chazzhorn1@gmail.com>
2020-08-30 14:38:58 -0500

    Updates

---
+++ precache-manifest.716cc5dd03d919e02875357e6a208e96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7c51b0e50360f9e516510d9ae44956",
+    "url": "/react-welcome-clock-demo1/index.html"
+  },
+  {
+    "revision": "55fcf1bd158c2b53ee89",
+    "url": "/react-welcome-clock-demo1/static/css/main.95624161.chunk.css"
+  },
+  {
+    "revision": "dc3c3f912ed665348333",
+    "url": "/react-welcome-clock-demo1/static/js/2.88d99b9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-welcome-clock-demo1/static/js/2.88d99b9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fcf1bd158c2b53ee89",
+    "url": "/react-welcome-clock-demo1/static/js/main.421b7e31.chunk.js"
+  },
+  {
+    "revision": "13b89b94b43a2e705de9",
+    "url": "/react-welcome-clock-demo1/static/js/runtime-main.9cb31d47.js"
+  },
+  {
+    "revision": "67212b62634fd30496299da73db0d355",
+    "url": "/react-welcome-clock-demo1/static/media/PurpleImg.67212b62.png"
+  }
+]);
